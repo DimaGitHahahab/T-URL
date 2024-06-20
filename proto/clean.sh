16adbaa8401811c381824e2374493516c97065d3
Dmitry Krasnoyarov <368378@niuitmo.ru>
2024-06-20 03:47:44 +0300

    feat: Add proto files, scripts for generation

---
+++ proto/clean.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf ../shortening-microservice/proto
+rm -rf ../redirection-microservice/proto
+rm -rf ../storage-microservice/proto
+rm -rf ../analytics-microservice/proto
+rm -rf ../api-gateway/proto
+
+rm -rf ./shortening
+rm -rf ./redirection
+rm -rf ./storage
+rm -rf ./analytics
